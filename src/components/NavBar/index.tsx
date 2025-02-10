0066a9db3bb97e6e310e6cce7d48ba126ff56391
beigeyoon <beige.yoon@gmail.com>
2025-02-11 01:29:35 +0900

    feat. NabBar shadow 추가

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -6,19 +6,32 @@ import Link from 'next/link';
 import Image from 'next/image';
 import HamburgerMenu from '../HamburgerMenu';
 import { NAV_BAR_MENU } from '@/constants';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const NavBar = () => {
   const [selectedMenu, setSelectedMenu] = useState<NAV_BAR_MENU>(
     NAV_BAR_MENU.FAQ
   );
+  const [hasShadow, setHasShadow] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasShadow(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="fixed top-0 flex h-[56px] w-full justify-between bg-white px-[48px] sm:px-[24px] lg:h-[80px]">
+    <div
+      className={`fixed top-0 z-10 flex h-[56px] w-full justify-between bg-white px-[48px] sm:px-[24px] lg:h-[80px] ${hasShadow ? 'shadow-[0_1px_25px_rgba(0,0,0,0.10)]' : ''}`}>
       <Link
         href="https://wiblebiz.kia.com/"
         className="flex items-center">
-        <div className="md-down:w-[120px] md-down:h-[40px] relative lg:h-full lg:w-[160px]">
+        <div className="relative lg:h-full lg:w-[160px] md-down:h-[40px] md-down:w-[120px]">
           <Image
             src="/images/wible_BIZ.svg"
             alt="wible_biz_logo"
